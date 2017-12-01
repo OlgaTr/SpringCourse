085a353a797be528fb566c4b4a61c282d90f629d
Olga Trubetckaia <olga.trubetckaia@mail.ru>
2017-12-01 23:36:57 +0300

    Config has been created.

--- src/main/java/screensaver/ColorFrame.java
+++ src/main/java/screensaver/ColorFrame.java
@@ -1,14 +1,16 @@
 package screensaver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
-public class ColorFrame extends JFrame {
-    @Autowired
-    private Color color;
+@Component
+public abstract class ColorFrame extends JFrame {
+
     public ColorFrame() {
         setSize(200, 200);
         setVisible(true);
@@ -18,7 +20,25 @@ public class ColorFrame extends JFrame {
     public void showOnRandomPlace() {
         Random random = new Random();
         setLocation(random.nextInt(1200), random.nextInt(700));
-        getContentPane().setBackground(color);
+        getContentPane().setBackground(getColor());
         repaint();
     }
+
+    protected abstract Color getColor();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
